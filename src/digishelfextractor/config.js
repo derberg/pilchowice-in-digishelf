ea48f8e7d238f4968a5af66fe2aa3f842ae7df13
Lukasz Gornicki <lpgornicki@gmail.com>
2025-01-18 13:55:00 +0100

    chore: new content

--- src/digishelfextractor/config.js
+++ src/digishelfextractor/config.js
@@ -1,29 +1,46 @@
-export const API_BASE_URL = 'https://digishelf.de/api';
-export const API_VERSION = 'v2';
-export const IGNORED_COLLECTIONS = ['allgemeinerbestand', 'altedrucke', 'altedruckerara', 'archiveimlandkreiskonstanz',
-  'bibliothek', 'christianeumhamburg', 'deutschebevlkerungsgruppenimausland', 'deutschesschiffahrtsmuseumbremerhaven', 'gleimhaus', 'handschriftensammlunggleims', 'historischedrucke', 'htwglimited', 'katalogehaum', 'kolonialismus', 'kolonialismuskolo', 'kunstbibliothekberlin'
+export const API_BASE_URL = "https://digishelf.de/api";
+export const API_VERSION = "v2";
+export const IGNORED_COLLECTIONS = [
+	"allgemeinerbestand",
+	"altedrucke",
+	"altedruckerara",
+	"archiveimlandkreiskonstanz",
+	"bibliothek",
+	"christianeumhamburg",
+	"deutschebevlkerungsgruppenimausland",
+	"deutschesschiffahrtsmuseumbremerhaven",
+	"gleimhaus",
+	"handschriftensammlunggleims",
+	"historischedrucke",
+	"htwglimited",
+	"katalogehaum",
+	"kolonialismus",
+	"kolonialismuskolo",
+	"kunstbibliothekberlin",
 ];
 export const KEYWORDS = [
-  'Nieborowice',
-  'Nieborowitz',
-  'Neubersdorf',
-  'Nieborowitz Hammer',
-  'Lebschowitz',
-  'Leboszowice',
-  'Wölka',
-  'Wolka',
-  'Stanitz',
-  'Wilcza',
-  'Stanica',
-  'Pilchowice',
-  'Pilchowitz',
-  'Żernica',
-  'Zernitz',
-  'Bilchengrund',
-  'Haselgrund',
-  'Klein-garben',
-  'Ungersch'
+	"Nieborowice",
+	"Nieborowitz",
+	"Neubersdorf",
+	"Nieborowitz Hammer",
+  "Nieborowitzhammer",
+	"Leboschowitz",
+	"Leboszowice",
+	"Wölka",
+	"Wolka",
+	"Stanitz",
+	"Wilcza",
+	"Stanica",
+	"Pilchowice",
+	"Pilchowitz",
+	"Żernica",
+	"Zernitz",
+	"Bilchengrund",
+	"Haselgrund",
+	"Klein-garben",
+	"Ungersch",
+  "Kleingarben"
 ];
 
-export const OUTPUT_DIR = 'extracted_content';
+export const OUTPUT_DIR = "extracted_content";
 export const CONCURRENT_REQUESTS = 5;
